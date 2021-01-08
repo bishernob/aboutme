f4f0da1217ac0606be698031507d5a850a646eaf
bishernobani96 <bisher-1996@hotmail.com>
2021-01-08 20:33:19 +0200

    create index file

---
+++ js/app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var user = prompt('What is your name ?');
+alert('Welcome'+user);
+var Name = prompt('your name is bisher nobani? Y/N');
+if(Name.toLowerCase() ==='y' || Name.toLowerCase() ==='yes')
+{
+    alert('correct');
+    //console.log('correct');
+}
+else{
+    //console.log('wrong');
+    alert('wrong');
+}
+var Age = prompt('Are you 27 ? Y/N');
+if(Age.toLowerCase() ==='y'|| Age.toLowerCase()==='yes')
+{
+    alert('correct');
+    // console.log('correct');
+}
+else{
+    // console.log('wrong');
+    alert('wrong');
+}
+var major = prompt ('Are you software eng ? Y/N');
+if(major.toLowerCase()==='y'|| major.toLowerCase()==='yes')
+{
+    
+    
+    alert('wrong');
+    // console.log('wrong');
+    
+}
+else{
+    alert('correct');
+    // console.log('correct');
+}
+var nationalty = prompt('Are you jordanian ? Y/N');
+if(nationalty.toLowerCase()==='y'|| nationalty.toLowerCase()==='yes')
+{
+    alert('correct');
+    // console.log('correct');
+}
+else{
+    console.log('wrong');
+    alert('wrong');
+}
+var car = prompt('My fav car ferrari? Y/N');
+if(car.toLowerCase()==='y'|| car.toLowerCase()==='yes')
+{
+    alert('correct');
+    // console.log('correct');
+}
+else{
+    // console.log('wrong');
+    alert('wrong');
+}
