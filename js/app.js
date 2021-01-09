c80d4bec3e5c5cafe697d6d27b111eb790802926
bishernobani96 <bisher-1996@hotmail.com>
2021-01-09 23:10:31 +0200

    add new questions

--- js/app.js
+++ js/app.js
@@ -1,81 +1,81 @@
-// 'use strict';
+'use strict';
 
-// var user = prompt('What is your name ?');
-// alert('Welcome'+user);
-// var Name = prompt('your name is bisher nobani? Y/N');
-// if(Name.toLowerCase() ==='y' || Name.toLowerCase() ==='yes')
-// {
-//     alert('correct');
-//     //console.log('correct');
-// }
-// else{
-//     //console.log('wrong');
-//     alert('wrong');
-// }
-// var Age = prompt('Are you 27 ? Y/N');
-// if(Age.toLowerCase() ==='y'|| Age.toLowerCase()==='yes')
-// {
+var user = prompt('What is your name ?');
+alert('Welcome'+user);
+var Name = prompt('your name is bisher nobani? Y/N');
+if(Name.toLowerCase() ==='y' || Name.toLowerCase() ==='yes')
+{
+    alert('correct');
+    //console.log('correct');
+}
+else{
+    //console.log('wrong');
+    alert('wrong');
+}
+var Age = prompt('Are you 27 ? Y/N');
+if(Age.toLowerCase() ==='y'|| Age.toLowerCase()==='yes')
+{
     
-//     alert('wrong');
-//     // console.log('wrong');
-// }
-// else{
-//     alert('correct');
-//     // console.log('correct');
+    alert('wrong');
+    // console.log('wrong');
+}
+else{
+    alert('correct');
+    // console.log('correct');
     
     
-// }
-// var major = prompt ('Are you software eng ? Y/N');
-// if(major.toLowerCase()==='y'|| major.toLowerCase()==='yes')
-// {
+}
+var major = prompt ('Are you software eng ? Y/N');
+if(major.toLowerCase()==='y'|| major.toLowerCase()==='yes')
+{
 
-//     alert('wrong');
-//     // console.log('wrong');
+    alert('wrong');
+    // console.log('wrong');
     
-// }
-// else{
-//     alert('correct');
-//     // console.log('correct');
-// }
-// var nationalty = prompt('Are you jordanian ? Y/N');
-// if(nationalty.toLowerCase()==='y'|| nationalty.toLowerCase()==='yes')
-// {
-//     alert('correct');
-//     // console.log('correct');
-// }
-// else{
-//     console.log('wrong');
-//     alert('wrong');
-// }
-// var car = prompt('My fav car ferrari? Y/N');
-// if(car.toLowerCase()==='y'|| car.toLowerCase()==='yes')
-// {
-//     alert('correct');
-//     // console.log('correct');
-// }
-// else{
-//     // console.log('wrong');
-//     alert('wrong');
-// }
+}
+else{
+    alert('correct');
+    // console.log('correct');
+}
+var nationalty = prompt('Are you jordanian ? Y/N');
+if(nationalty.toLowerCase()==='y'|| nationalty.toLowerCase()==='yes')
+{
+    alert('correct');
+    // console.log('correct');
+}
+else{
+    console.log('wrong');
+    alert('wrong');
+}
+var car = prompt('My fav car ferrari? Y/N');
+if(car.toLowerCase()==='y'|| car.toLowerCase()==='yes')
+{
+    alert('correct');
+    // console.log('correct');
+}
+else{
+    // console.log('wrong');
+    alert('wrong');
+}
 
-// var exp = prompt('How many years of experiance in android programming'); 
-// //exp = parseInt(exp,10);
-// var answer_6 = 0;
-// while(!exp===2 || answer<4)
-// {   
-//     if(parseInt(exp) > 2) {
-//         alert('Too high');
-//         console.log(exp);
-//         exp = prompt('How many years of experiance in android programming');
-//         answer++;
-//     } 
-//     else if(parseInt(exp)<2){
-//         alert('Too low');
-//         console.log(exp);
-//         exp = prompt('How many years of experiance in android programming');
-//         answer++;
-//     }    
-// }
+var exp = prompt('How many years of experiance in android programming'); 
+//exp = parseInt(exp,10);
+var answer_6 = 0;
+while(!exp===2 || answer<4)
+{   
+    if(parseInt(exp) > 2) {
+        alert('Too high');
+        console.log(exp);
+        exp = prompt('How many years of experiance in android programming');
+        answer++;
+    } 
+    else if(parseInt(exp)<2){
+        alert('Too low');
+        console.log(exp);
+        exp = prompt('How many years of experiance in android programming');
+        answer++;
+    }    
+}
 
 var attempt = 0;
 var exper = false;
@@ -123,7 +123,7 @@ for (var x=0; x<6;x++)
         }
 }
 
-    alert('here is the right answers ' + fav_country);
+alert('here is the right answers ' + fav_country);
 
 
 
